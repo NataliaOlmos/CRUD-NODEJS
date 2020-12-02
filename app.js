b0c535ced399198f5577784d248ef40fc76e0d4e
Natalia <nataliav_4428@hotmail.com>
2020-12-01 22:42:07 -0600

    connected mongoose

--- app.js
+++ app.js
@@ -2,26 +2,22 @@ const express = require('express');
 const app = express();
 const mongoose = require ('mongoose')
 require('dotenv').config()
+const uri = 'mongodb://localhost:27017/test'
 
 const bodyPaser = require('body-parser');
 
-//MIDDLEWARE
+// //MIDDLEWARE
 app.use(bodyPaser.urlencoded({extended: false}));
 app.use(bodyPaser.json())
 app.use(express.static(__dirname + '/public'));
 
-const password = process.env.PASSWORD;
-const user = process.env.USER;
-const url = process.env.URL;
-const dbName = process.env.DBNAME;
-const uri = `mongodb://${user}:${password}@${url}/${dbName}`
-mongoose.connect(uri , {useNewUrlParser: true, useUnifiedTopology: true }
-    )
-    const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-});
+
+
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connection.on('open', _ =>{
+    console.log('Database is connected to', uri)
+})
 
 app.get('/about', (req, res) => {
     res.send('About section');
@@ -32,3 +28,4 @@ app.get('/about', (req, res) => {
 app.listen(process.env.PORT || 3000, () => { 
     console.log("Servidor iniciado en");
 });
+
