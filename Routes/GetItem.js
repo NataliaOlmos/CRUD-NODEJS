40bd75fd724b62833f3a75feff82c93085bec94f
Natalia <nataliav_4428@hotmail.com>
2020-12-12 23:19:59 -0600

    Show images, edit and delete screen

--- Routes/GetItem.js
+++ Routes/GetItem.js
@@ -1,3 +1,16 @@
-module.exports = (req, res) => {
-    res.send(`id:${req.params.id}`);
+const Foto = require('../models/fotos')
+module.exports = async (req, res) => {
+    const { id } = req.params
+    console.log(id);
+    const query = Foto.where({_id: id})
+    const item = await query.findOne();
+    console.log(item.filename);
+//     query.findOne((err, item) => {
+// if (err) return handleError(err);
+ res.render('getitem',{ 
+     item: item
+})
+    ;
+//     });
+    //res.send(`id:${req.params.id}`);
 }
